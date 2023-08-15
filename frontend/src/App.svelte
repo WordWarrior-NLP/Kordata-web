8399e6c24e3e5ed49a7b1e9bfb6c83ec7febb08a
smreosms13 <smreosms13@gamil.com>
2023-08-15 19:23:15 +0900

    Feat : Make page 'NotFound' for invalid routes access and adjest in App

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,9 +3,12 @@
   import Home from "./routes/Home.svelte"
   import Navbar from "./components/Navbar.svelte";
   import Footer from "./components/Footer.svelte";
+  import NotFound from './routes/NotFound.svelte';
+  import Event from './routes/Event.svelte';
   const routes = {
     '/': Home,
-    // '/Event': Diagram,
+    "/events/:id" : Event,
+    '*' : NotFound,
   }
 </script>
 <Navbar/>
---
+++ frontend/src/routes/NotFound.svelte
@@ -0,0 +1,6 @@
+<section class="page-section bg-light" id="notFound">
+    <div class="container">
+        <h2 class="routetitle">Page Not Found 404</h2>
+        <p>Oops, this route doesn't exist!</p>
+    </div>
+</section>
