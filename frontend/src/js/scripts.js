ed80690b57fa406ec4f5b720e9534880ed11e9f1
smreosms13 <smreosms13@gamil.com>
2023-08-17 14:35:41 +0900

    fix : remove the unnecessary 'EventListener' for toggle of sidebar

--- frontend/src/js/scripts.js
+++ frontend/src/js/scripts.js
@@ -49,23 +49,6 @@ window.addEventListener('DOMContentLoaded', event => {
                 navbarToggler.click();
             }
         });
-    });
-
-
-
-    // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
-    if (sidebarToggle) {
-        // Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-sidenav-toggled');
-        // }
-        sidebarToggle.addEventListener('click', event => {
-            event.preventDefault();
-            document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', String(document.body.classList.contains('sb-sidenav-toggled')));
-        });
-    }
-    
+    });    
 
 });
