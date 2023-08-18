1a5dacd71bfeef7328137c8b5db43fddcc724479
smreosms13 <smreosms13@gamil.com>
2023-08-19 05:25:50 +0900

    design : adjust style at Sidebar container and items

--- frontend/src/components/Sidebar.svelte
+++ frontend/src/components/Sidebar.svelte
@@ -1,8 +1,8 @@
 <div class="border-end bg-white d-flex flex-column align-items-stretch flex-shrink-0" id="sidebar-wrapper">
-  <div class="sidebar-heading border-bottom bg-light"><span class="fs-5 fw-semibold">Articles</span></div>
+  <div class="sidebar-heading border-bottom"><span class="fs-5 fw-semibold">Articles</span></div>
   <div class="list-group list-group-flush border-bottom scrollarea">
       {#each days as day, index}
-          <a href="#" class="list-group-item list-group-item-action active py-3 lh-tight list-group-item-light" aria-current="true">
+          <a href="#app" class="list-group-item list-group-item-action py-3 lh-tight" aria-current="true">
               <div class="d-flex w-100 align-items-center justify-content-between">
                   <strong class="mb-1">List group item heading</strong>
                   <small>{index}</small>
