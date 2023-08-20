b12c123b754af09033f392d839c36b68e0ccfd13
smreosms13 <smreosms13@gamil.com>
2023-08-21 02:02:38 +0900

    feat : make page layout for graph and sidebar showing list of news related event
    Diagram - make function to request list of news with ID of event and layout of page
    Sidebar - split Sidebar component from Diagram

--- frontend/src/components/Sidebar.svelte
+++ frontend/src/components/Sidebar.svelte
@@ -1,17 +1,19 @@
 <div class="border-end bg-white d-flex flex-column align-items-stretch flex-shrink-0" id="sidebar-wrapper">
   <div class="sidebar-heading border-bottom"><span class="fs-5 fw-semibold">Articles</span></div>
   <div class="list-group list-group-flush border-bottom scrollarea">
-      {#each days as day, index}
-          <a href="#app" class="list-group-item list-group-item-action py-3 lh-tight" aria-current="true">
-              <div class="d-flex w-100 align-items-center justify-content-between">
-                  <strong class="mb-1">List group item heading</strong>
-                  <small>{index}</small>
-              </div>
-              <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
-          </a>
+    {#each news_list as cluster}
+      {#each cluster.news as news}
+        <a href={news.linkUrl} target="_blank" class="list-group-item list-group-item-action py-3 lh-tight" aria-current="true">
+          <div class="col-11 mb-1 small"><strong>{news.title}</strong></div>
+          <div class="d-flex w-100 align-items-center justify-content-between">
+            <small class="mb-1">{news.pid}</small>
+            <small>{cluster.datetime}</small>
+          </div>
+        </a>  
       {/each}
+    {/each}
   </div>
 </div>
 <script>
-  let days = ["sun", 'mon', 'tue', 'wed', 'thu', 'fri', 'sun']
+  export let news_list;
 </script>
--- frontend/src/routes/Diagram.svelte
+++ frontend/src/routes/Diagram.svelte
@@ -1,11 +1,40 @@
 <script>
-    import Sidebar from "../components/Sidebar.svelte";
-    let sideActive = false;
+  import { Link } from "svelte-routing";
+  import Sidebar from "../components/Sidebar.svelte";
+
+  let sideActive = false;
+
+  export let cid;
+  // let cid = parseInt(params.cid)
+  console.log(cid)
+  import fastapi from "../lib/api";    
+
+  let news_list = []
+  function getNewsFromEvent() {
+    return new Promise((resolve, reject) => {
+      fastapi('get', '/api/events/' +cid+'/news', {}, (json) => {
+        resolve(json);
+        reject(json)
+      });
+    });
+  }
+
+  async function get_news_list() {
+      try {
+        let json = await getNewsFromEvent();
+        news_list = [...news_list, ...json];
+      } catch (error) {
+        console.error("Error fetching news:", error);
+      }
+    }
+
+  get_news_list()
+
 </script>
 <section class="bg-light" id="event">
   <div class="d-flex h-100" id="wrapper" class:toggled={sideActive}>
     <!-- Sidebar -->
-     <Sidebar></Sidebar>
+     <Sidebar news_list={news_list}></Sidebar>
     <!-- Page content wrapper-->
     <div id="page-content-wrapper">
         <!-- Top navigation-->
@@ -15,17 +44,8 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-                        <li class="nav-item active"><a class="nav-link" href="#!">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Link</a></li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#app" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#!">Action</a>
-                                <a class="dropdown-item" href="#!">Another action</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#!">Something else here</a>
-                            </div>
-                        </li>
+                        <li class="nav-item active"><Link class="nav-link" to="/">Home</Link></li>
+                        <li class="nav-item"><Link class="nav-link" to="/events">Events</Link></li>
                     </ul>
                 </div>
             </div>
