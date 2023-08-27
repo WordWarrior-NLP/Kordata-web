ca90c83b2fb81fb8f383e534f6fd8c787d91ead1
smreosms13 <smreosms13@gamil.com>
2023-08-28 03:24:55 +0900

    fix : get newest news_cluster.datetime related entity

--- backend/routers/graph.py
+++ backend/routers/graph.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from database import get_db
-from sqlalchemy.orm import Session,joinedload,load_only
+from sqlalchemy import func
+from sqlalchemy.orm import Session
 from internal.crud import *
 from model import Event, NewsCluster, News, Entity, Sentiment
 from internal.schema import *
@@ -12,6 +13,13 @@ async def get_graph_data(
         cid: int,
         db: Session = Depends(get_db)
 ):
+    entity_data = (
+        db.query(Entity.eid, Entity.nc_id, Entity.main_word, NewsCluster.cid, func.max(NewsCluster.datetime))
+        .join(NewsCluster, Entity.nc_id == NewsCluster.nc_id)
+        .filter(NewsCluster.cid == cid)
+        .group_by(Entity.main_word)
+        .all()
+    )
     nc_ids = db.query(NewsCluster.nc_id).filter_by(cid=cid).all()
     nc_ids = [nc_id[0] for nc_id in nc_ids]
     news_data = db.query(News.title, News.nid, News.nc_id, News.datetime, Entity.main_word, Entity.datetime, Sentiment.polarity) \
@@ -36,25 +44,33 @@ async def get_graph_data(
         }
         event_node = Node(event.cid, 0, event_attributes, end_date)
         output.append(event_node)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
 
-    if news_data:
-        for news in news_data:
-            main_word = news[4]
+    if entity_data:
+        print(entity_data[0])
+        for entity in entity_data:
+            eid = entity[0]
+            nc_id = entity[1]
+            main_word = entity[2]
+            cid = entity[3]
+            datetime = entity[4]
             if main_word not in existed_et:
                 existed_et.append(main_word)
                 entity_cnt += 1
                 entity_attributes = {
                     'label': main_word,
-                    'cluster': news[2],
-                    'datetime': news[5],
+                    'cluster': nc_id,
+                    'datetime': datetime,
                 }
-
                 entity_node = Node(entity_cnt, 1, entity_attributes, end_date)
                 output.append(entity_node)
                 entity_key = entity_node.data['id']
                 edge_id += 1
                 entity_to_event = Edge(edge_id, entity_key, output[0].data['id'])
                 output.append(entity_to_event)
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
 
     if news_data:
         for news in news_data:
@@ -79,4 +95,7 @@ async def get_graph_data(
             target = "entity_" + str(existed_et.index(news[4]))
             news_to_entity = Edge(edge_id, source, target, news_to_entity_attr)
             output.append(news_to_entity)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
+
     return output
